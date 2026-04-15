36c605652c2b380fee4637fed3e426f62f688a67
Vadym <vados199368@gmail.com>
2026-04-15 14:59:19 +0300

    feat: add userRankSchema and corresponding type for user ranking

--- src/api/users/users.schemas.ts
+++ src/api/users/users.schemas.ts
@@ -11,3 +11,10 @@ export const usersSchema = z.array(userSchema);
 export const createUserBodySchema = z.object({
   username: z.string().trim().min(3).max(100),
 });
+
+export const userRankSchema = z.object({
+  user_id: z.number(),
+  username: z.string(),
+  rank: z.number(),
+  total_score: z.number(),
+});
--- src/api/users/users.types.ts
+++ src/api/users/users.types.ts
@@ -1,5 +1,10 @@
 import z from "zod";
-import { createUserBodySchema, userSchema } from "./users.schemas";
+import {
+  createUserBodySchema,
+  userRankSchema,
+  userSchema,
+} from "./users.schemas";
 
 export type UserT = z.infer<typeof userSchema>;
 export type CreateUserBodyT = z.infer<typeof createUserBodySchema>;
+export type UserRankT = z.infer<typeof userRankSchema>;
